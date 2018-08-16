ec3fe0244905b71b2ec0b32ff4e5f8ce021c6f3d
MajaMarkovic1 <markovicm9000@gmail.com>
2018-08-16 20:02:57 +0200

    zadatak 1

---
+++ src/components/Navbar.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav mr-auto">
+            <li class="nav-item active">
+              <div class="nav-link">Blog app</div>
+            </li>
+            <li class="nav-item active">
+              <router-link class="nav-link" to="/posts">Posts</router-link>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </div>
+</template>
+
+<style>
+nav div {
+  background-color: rgb(207, 58, 58);
+}
+
+</style>
---
+++ src/containers/AppPosts.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <Posts />
+    </div>
+</template>
+
+<script>
+import Posts from '../components/Posts.vue'
+
+export default {
+    name: 'AppPosts',
+    components: {
+        Posts,
+        
+    }
+
+
+}
+
+</script>
+
+<style>
+
+</style>
